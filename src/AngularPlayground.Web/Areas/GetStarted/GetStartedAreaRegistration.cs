9f228ea200e2111a06a6f5794a83a921ff076654
Nuno Costa <nunofilipe.costa@gmail.com>
2016-09-04 22:03:08 +0100

    Add project files.

---
+++ src/AngularPlayground.Web/Areas/GetStarted/GetStartedAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AngularPlayground.Web.Areas.GetStarted
+{
+    public class GetStartedAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "GetStarted";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "GetStarted_default",
+                "GetStarted/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/AngularPlayground.Web/Scripts/App/patterns.js
@@ -0,0 +1,37 @@
+﻿
+// IIFE - immediately invoked function expression
+(function () {
+
+
+    // pattern : revealing module patter
+
+    var createWorker = function () {
+
+        // private
+        var workCount = 0;
+
+        var task1 = function () {
+            workCount += 1;
+            console.log("task1");
+        };
+
+        var task2 = function () {
+            workCount += 1;
+            console.log("task2");
+        };
+
+        // public
+        return {
+            job1: task1,
+            job2: task2
+        };
+
+    };
+
+
+    // concept
+    var worker = createWorker();
+    worker.job1();
+    worker.job2();
+
+}());
